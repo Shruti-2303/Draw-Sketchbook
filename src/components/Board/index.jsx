da95faf3b97ddc289f03810e95019827b46dbc69
Shruti-2303 <sharmas23032001@gmail.com>
2023-12-26 19:52:43 +0530

    Feat : Added Undo and Redo Feature

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
+  const drawHistory = useRef([]);
+  const historyPointer = useRef(null);
   const shouldDraw = useRef(false);
   const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
@@ -22,9 +24,21 @@ const Board = () => {
       a.href = URL;
       a.download = "sketch.png";
       a.click();
+    } else if (
+      actionMenuItem === MENU_ITEMS.UNDO ||
+      actionMenuItem === MENU_ITEMS.REDO
+    ) {
+      if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO)
+        historyPointer.current -= 1;
+      if (
+        historyPointer.current < drawHistory.current.length - 1 &&
+        actionMenuItem === MENU_ITEMS.REDO
+      )
+        historyPointer.current += 1;
+      const imageData = drawHistory.current[historyPointer.current];
+      context.putImageData(imageData, 0, 0);
     }
     dispatch(actionItemClick(null));
-    console.log("actionMenuItem", actionMenuItem);
   }, [actionMenuItem, dispatch]);
 
   useEffect(() => {
@@ -67,6 +81,9 @@ const Board = () => {
     };
     const handleMouseUp = (e) => {
       shouldDraw.current = false;
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length - 1;
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
