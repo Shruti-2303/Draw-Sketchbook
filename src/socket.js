6dd9674640e7a75992b7dbd82b031b9fc4409578
Shruti-2303 <sharmas23032001@gmail.com>
2023-12-27 18:32:50 +0530

    Added server endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:8000/");
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://sketchsync-server.onrender.com/"
+    : "http://localhost:8000/";
+export const socket = io(URL);
